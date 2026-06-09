1e7f8c29464fdcf6769f95486c4a6b0d2cd7ebc8
Yasmin Atwa <yasminatwa@Yasmins-MacBook-Air-2.local>
2026-06-09 11:06:05 +0400

    Initial Next.js skeleton

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white p-8">
+      <h1 className="text-4xl font-bold">fixation</h1>
+      <p className="text-gray-400">Loading...</p>
+    </div>
+  )
+}
